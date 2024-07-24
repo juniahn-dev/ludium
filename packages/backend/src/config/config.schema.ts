4adaee20f049baca5900f1d85c52d6361cccb260
juniahn-dev <gud0415@gmail.com>
2024-07-24 13:21:11 -0700

    [chore] Add env file in backend

--- packages/backend/src/config/config.schema.ts
+++ packages/backend/src/config/config.schema.ts
@@ -1,3 +1,14 @@
 import * as Joi from 'joi';
 
-export const envValidationSchema = Joi.object({});
+export const envValidationSchema = Joi.object({
+  SESSION_SECRET_KEY: Joi.string().required(),
+  SESSION_EXPIRES_IN: Joi.number().required(),
+
+  COOKIE_NAME: Joi.string().required(),
+
+  LOCAL_SERVICE_DB_HOST: Joi.string().required(),
+  LOCAL_SERVICE_DB_DATABASE: Joi.string().required(),
+  LOCAL_SERVICE_DB_USER: Joi.string().required(),
+  LOCAL_SERVICE_DB_PASSWORD: Joi.string().required(),
+  LOCAL_SERVICE_DB_PORT: Joi.number().required(),
+});
--- packages/backend/src/middlewares/logger.middleware.ts
+++ packages/backend/src/middlewares/logger.middleware.ts
@@ -10,8 +10,6 @@ import { Logger } from '@nestjs/common';
 export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger('LoggerMiddleware');
 
-  //   constructor(private readonly loggerService: LoggerService) {}
-
   use(req: Request, res: Response, next: NextFunction) {
     const { ip, method, originalUrl, baseUrl, headers, query } = req;
 
@@ -48,46 +46,6 @@ export class LoggerMiddleware implements NestMiddleware {
         // Token 들의 로그 및 호출 횟수 기록 로직
         console.log(token);
         // const context = req.context;
-
-        // const is5xxError = statusCode >= 500 && statusCode <= 599;
-        // const isNotAcceptable = statusCode === HttpStatus.NOT_ACCEPTABLE;
-
-        // const usedCredits =
-        //   is5xxError || isNotAcceptable ? 0 : Number(context?.credits || 3);
-        // const moduleType = context?.moduleType || ModuleTypes.NONE;
-
-        // const existToken =
-        //   await this.loggerService.getAccessTokenByToken(token);
-
-        // if (existToken) {
-        //   const writeHistoryPromise = this.loggerService.writeTokenHistory({
-        //     userId: existToken.userId,
-        //     accessTokenId: Number(existToken.id),
-        //     url: originalUrl,
-        //     method,
-        //     statusCode: String(statusCode),
-        //     query: queryString,
-        //     credits: usedCredits,
-        //     responseTime,
-        //     moduleType,
-        //   });
-
-        //   const updateStatsPromise = this.loggerService.updateTokenStats(
-        //     existToken,
-        //     statusCode,
-        //     usedCredits,
-        //   );
-
-        //   const updateUserTokenStatisticsPromise =
-        //     this.loggerService.updateUserTokenStatistics(
-        //       existToken,
-        //       usedCredits,
-        //     );
-
-        //   await writeHistoryPromise;
-        //   await updateStatsPromise;
-        //   await updateUserTokenStatisticsPromise;
-        // }
       }
     });
 
